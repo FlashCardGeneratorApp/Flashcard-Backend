60a078ba906e176a6a01db52a3e8bbb2215ee70a
Quinten Leung <wleung105@my.bcit.ca>
2023-11-29 16:44:17 -0800

    working on endpoints

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from utils import client, dbname, collection_name  # for Azure Cosmos DB
 import json
+from  questionGenerator import generate_questions
 from bson import ObjectId
 
 app = Flask(__name__)
@@ -14,41 +15,18 @@ def create_new_note():
             return jsonify({"message": "Missing User ID"},500)
         for question in note_data.questions:
             question.user_id = note_data.user_id
-            result = collection_name.insert_one(note_data)
-        return jsonify({"message": "Note created successfully"})
+        result = collection_name.insert_many(note_data)
+        return jsonify({"message": "Notes created successfully"})
     except Exception as e:
         return str(e), 500
 
-# Get all notes
-@app.route('/notes/', methods=['GET'])
-def get_all_notes():
+# Get all notes associated with a user
+@app.route('/notes/<user_id>', methods=['GET'])
+def get_all_notes(user_id):
     try:
         notes_cursor = collection_name.find()
         notes = list(notes_cursor)
         return json.dumps(notes, default=str), 200, {'Content-Type': 'application/json'}
-    except Exception as e:
-        return str(e), 500
-
-# Get note by ID
-@app.route('/notes/<note_id>', methods=['GET'])
-def get_note_by_id(note_id):
-    try:
-        note = collection_name.find_one({'_id': ObjectId(note_id)})
-        if note:
-            note['_id'] = str(note['_id'])
-            return jsonify(note)
-        else:
-            return jsonify({'error': 'Note not found'}), 404
-    except Exception as e:
-        return str(e), 500
-
-# Update note
-@app.route('/notes/<note_id>', methods=['PUT'])
-def update_note(note_id):
-    try:
-        update_data = request.get_json()
-        collection_name.update_one({'_id': ObjectId(note_id)}, {'$set': update_data})
-        return jsonify({'message': 'Note updated successfully'})
     except Exception as e:
         return str(e), 500
 
@@ -64,15 +42,10 @@ def delete_note(note_id):
     except Exception as e:
         return str(e), 500
 
-# Notes handler for both POST and GET requests to the same URL
-@app.route('/notes/', methods=['POST', 'GET'])
-def notes_handler():
-    if request.method == 'POST':
-        return create_new_note()
-    elif request.method == 'GET':
-        return get_all_notes()
-    else:
-        return jsonify({'error': 'Invalid HTTP method'}), 405
+
+@app.route('/notes/generate/<topic>', methods=['GET'])
+def question_helper(topic):
+    return generate_questions(topic)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
